0e44f4532711232214fb3fbfcf9722d448723af9
zaphodess <monika.messerer@telekom.de>
2020-01-20 14:54:38 +0100

    Jdbc1

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,7 +1,16 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
+import com.wildcodeschool.wildandwizard.util.JdbcUtils;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SchoolRepository {
@@ -13,18 +22,117 @@ public class SchoolRepository {
     public List<School> findAll() {
 
         // TODO : find all schools
+    	
+    	Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "SELECT * FROM school;"
+            );
+            resultSet = statement.executeQuery();
+
+            List<School> schools = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                String country = resultSet.getString("country");
+      
+                schools.add(new School(id, name, capacity, country));
+            }
+            return schools;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResultSet(resultSet);
+            JdbcUtils.closeStatement(statement);
+            JdbcUtils.closeConnection(connection);
+        }
+    	
+    	// END code by mme
         return null;
     }
 
     public School findById(Long id) {
 
         // TODO : find a school by id
+    	 Connection connection = null;
+         PreparedStatement statement = null;
+         ResultSet resultSet = null;
+         try {
+             connection = DriverManager.getConnection(
+                     DB_URL, DB_USER, DB_PASSWORD
+             );
+             statement = connection.prepareStatement(
+                     "SELECT * FROM school WHERE id = ?;"
+             );
+             statement.setLong(1, id);
+             resultSet = statement.executeQuery();
+            
+
+
+             if (resultSet.next()) {
+            	 
+                 String name = resultSet.getString("name");
+                 Long capacity = resultSet.getLong("capacity");
+                 String country = resultSet.getString("country");
+                 return new School (id, name, capacity, country);
+
+             }
+             
+         } catch (SQLException e) {
+             e.printStackTrace();
+         } finally {
+             JdbcUtils.closeResultSet(resultSet);
+             JdbcUtils.closeStatement(statement);
+             JdbcUtils.closeConnection(connection);
+         }
+    	
+    	// End TODO
         return null;
     }
 
     public List<School> findByCountry(String country) {
 
         // TODO : search schools by country
+    	Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "SELECT * FROM school WHERE country LIKE ?;"
+            );
+            statement.setString(1, country);
+            resultSet = statement.executeQuery();
+           
+            List<School> schools = new ArrayList<>();
+
+            while (resultSet.next()) {
+            	Long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                Long capacity = resultSet.getLong("capacity");
+                country = resultSet.getString("country");
+      
+                schools.add(new School(id, name, capacity, country));
+            }
+            return schools;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResultSet(resultSet);
+            JdbcUtils.closeStatement(statement);
+            JdbcUtils.closeConnection(connection);
+        }
+   	
+   	// End TODO
         return null;
     }
 }
